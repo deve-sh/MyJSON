7297b2203a5bded4f67bdaf8d277e146e8f6a59c
Devesh Kumar <devesh2027@gmail.com>
2019-10-08 22:23:20 +0530

    Added the header.

--- Frontend/src/components/header/header.js
+++ Frontend/src/components/header/header.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Header = props => {
-  return <></>;
+  return (
+    <div id="header">
+      <div className="fixedContainer row">
+        <div className="col-6">MyJSON</div>
+        <div className="col-6 alignright">
+          <i
+            className="fas fa-adjust fa-lg darkmodebutton"
+            onClick={props.toggleDarkMode}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
