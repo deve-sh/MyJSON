ef3c6233d6c4aab705a9972ba1253aa9ae315574
Devesh Kumar <devesh2027@gmail.com>
2022-10-15 13:09:32 +0530

    Frontend: Updated backend URL in config

--- Frontend/src/config/index.js
+++ Frontend/src/config/index.js
@@ -1,14 +1,14 @@
 // File for all the configuration files.
 
 const config = {
-  HTTPS: "https://",
-  HTTP: "http://",
-  BACKENDURL: "localhost:4567",
-  REMOTEBACKEND: "myjson.herokuapp.com",
-  GETJSON: "/api/getjson/",
-  TEXTAREANAME: "jsontosend",
-  TEXTAREAMODE: "javascript",
-  TEXTAREATHEME: "monokai"
+	HTTPS: "https://",
+	HTTP: "http://",
+	BACKENDURL: "localhost:4567",
+	REMOTEBACKEND: "myjson-backend.vercel.app",
+	GETJSON: "/api/getjson/",
+	TEXTAREANAME: "jsontosend",
+	TEXTAREAMODE: "javascript",
+	TEXTAREATHEME: "monokai",
 };
 
 export default config;
